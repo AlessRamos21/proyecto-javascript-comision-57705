bd4bc0b7e1657cc2734340d66776efc04a90a63f
alessandro <raless218@gmail.com>
2024-05-18 14:42:38 -0300

    creacion de script, modificacion de estilos y html

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function convertir() {
+    let valore = parseInt(document.getElementById("valor").value);
+    let resultado = 0;
+    let dolar = 905.50;
+    let euro = 962.48;
+    if(document.getElementById("uno")){
+        resultado = valore / dolar;
+        alert("El cambio de Pesos a Dolares es: $" + resultado.toFixed(2));
+    }
+    else if(document.getElementById("dos")){
+        resultado = valore / euro;
+        alert("El cambio de Pesos a Euros es: " + resultado.toFixed(2));
+    }
+    else{
+        alert("Tenes que completar todos los espacios")
+    }
+}
