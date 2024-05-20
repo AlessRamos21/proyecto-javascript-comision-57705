6db97642ab25bf97b8f5d9ad990626f83dc51f0a
alessandro <raless218@gmail.com>
2024-05-20 17:59:12 -0300

    modificacion del script

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ function convertir() {
     let resultado = 0;
     let dolar = 905.50;
     let euro = 962.48;
-    if(document.getElementById("uno")){
+    if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
         alert("El cambio de Pesos a Dolares es: $" + resultado.toFixed(2));
     }
-    else if(document.getElementById("dos")){
+    else if (document.getElementById("dos").checked) {
         resultado = valore / euro;
         alert("El cambio de Pesos a Euros es: " + resultado.toFixed(2));
     }
-    else{
+    else {
         alert("Tenes que completar todos los espacios")
     }
 }
